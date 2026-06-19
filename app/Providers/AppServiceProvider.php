1d8575a84874cb11c7b8fa587c679ba3ae244f75
Ditya <gdevelop351@gmail.com>
2026-06-19 21:14:38 +0800

    Add HTTPS enforcement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('id');
+        
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
